19a9e1c97ca56805b36a1869b6476d4888cd5859
Luiz Gasparetto <93852243+luizgasp@users.noreply.github.com>
2021-12-30 19:06:59 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Giphy Developer',
+      title: 'Giphy',
       home: const HomePage(),
       theme: ThemeData(
         hintColor: Colors.white,
